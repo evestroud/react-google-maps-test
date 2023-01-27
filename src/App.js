e7a010f45d763e4ee118d3d042df70aeaf1fd794
Eve Stroud <evestroud89@gmail.com>
2023-01-27 17:57:15 -0500

    Add simple Map view (no functionality)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+/* global google */
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import "./App.css";
 
+const MAPS_API_KEY = `${process.env.REACT_APP_MAPS_API_KEY}`;
+
 function App() {
-  return <div className="App"> </div>;
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: MAPS_API_KEY,
+  });
+
+  return (
+    <div className="App">
+      {isLoaded ? (
+        <GoogleMap
+          zoom={9}
+          center={{ lat: -4, lng: 40.7 }}
+          mapContainerClassName="map-container"
+        />
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
